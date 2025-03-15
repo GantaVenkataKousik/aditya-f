671df6d0dc150a09395750be4fbd3ff9af400b58
GVK <venkatakousikcse01@gmail.com>
2025-03-15 14:18:11 +0530

    feat: new changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
     const fetchUser = async () => {
       try {
         const token = localStorage.getItem('token');
-        const response = await fetch('http://localhost:5000/fetchData', {
+        const response = await fetch('https://aditya-b.onrender.com/fetchData', {
           method: 'GET',
           credentials: 'include',
           headers: {
